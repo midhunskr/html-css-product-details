1ec0875910a8f7cf1b06fd4cd6a6fcbace035898
midhunskr <midhun.shankar.199@gmail.com>
2024-07-16 23:20:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('add-to-cart').addEventListener('click', function() {
+    const size = document.getElementById('size').value;
+    alert(`Added Awesome Shoe (Size: ${size}) to your cart!`);
+});
